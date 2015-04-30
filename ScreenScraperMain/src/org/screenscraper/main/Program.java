52dde43bc844072e42988df8e988a02b133a3940
Paul Marina <paul.marina@fortech.ro>
2015-04-30 15:49:03 +0300

    First program

---
+++ ScreenScraperMain/src/org/screenscraper/main/Program.java
@@ -0,0 +1,16 @@
+package org.screenscraper.main;
+
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String args[]) {
+		
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
