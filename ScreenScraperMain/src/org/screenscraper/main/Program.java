9e86ffded62c9b1ff4f7b642a815a2a05a6496f2
alinamoldovan90 <alina.moldovan@fortech.ro>
2015-05-04 17:46:14 +0300

    worked on main page parser

--- ScreenScraperMain/src/org/screenscraper/main/Program.java
+++ ScreenScraperMain/src/org/screenscraper/main/Program.java
@@ -1,19 +1,36 @@
 package org.screenscraper.main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+
+import org.screenscraper.parsers.*;
+import org.screenscraper.objects.*;
 
 public class Program {
 
 	public static void main(String args[]) {
 
-		 
-
 		System.out.println("cevaaaaaaaa");
-		/*try {
-			System.in.read();
+		
+		StringBuilder contentBuilder = new StringBuilder();
+		try {
+		    BufferedReader in = new BufferedReader(new FileReader("C:/Users/ALINAM/Desktop/sss.html"));
+		    String str;
+		    while ((str = in.readLine()) != null) {
+		        contentBuilder.append(str);
+		    }
+		    in.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		}
+		String content = contentBuilder.toString();
+		
+		MainPageHtmlParser mphp = new MainPageHtmlParser();
+		List<CompanyUrl> companyUrls = mphp.parse(content);
+		
+		for (CompanyUrl companyUrl : companyUrls) {
+			System.out.println(companyUrl.getUrl());
+		}
 	}
 }
