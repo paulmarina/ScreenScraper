8c88c6096afa0e192a02ef073bdbe0bd8a86dd90
ioanafoidas <ioana.foidas@fortech.ro>
2015-05-04 17:46:04 +0300

    modified read method

--- ScreenScraperMain/src/org/screenscraper/excel/ReadWriteExcelFile.java
+++ ScreenScraperMain/src/org/screenscraper/excel/ReadWriteExcelFile.java
@@ -14,6 +14,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.screenscraper.objects.Company;
 
 public class ReadWriteExcelFile {
 
@@ -35,24 +36,33 @@ public class ReadWriteExcelFile {
 				Row row = rowIterator.next();
 
 				// For each row, iterate through each columns
-				Iterator<Cell> cellIterator = row.cellIterator();
-				while (cellIterator.hasNext()) {
+				/*
+				 * Iterator<Cell> cellIterator = row.cellIterator(); while
+				 * (cellIterator.hasNext()) {
+				 * 
+				 * Cell cell = cellIterator.next();
+				 * 
+				 * switch (cell.getCellType()) {
+				 * 
+				 * 
+				 * case Cell.CELL_TYPE_NUMERIC: System.out.print((int)
+				 * (cell.getNumericCellValue()) + "\t\t"); break; case
+				 * Cell.CELL_TYPE_STRING:
+				 * System.out.print(cell.getStringCellValue() + "\t\t"); break;
+				 * }
+				 * 
+				 * 
+				 * 
+				 * }
+				 */
 
-					Cell cell = cellIterator.next();
+				for (int i = 0; i < 14; i++) {
+					
+					Cell cell = row.getCell(i);
+					
 
-					switch (cell.getCellType()) {
-					case Cell.CELL_TYPE_BOOLEAN:
-						System.out.print(cell.getBooleanCellValue() + "\t\t");
-						break;
-					case Cell.CELL_TYPE_NUMERIC:
-						System.out.print((int) (cell.getNumericCellValue())
-								+ "\t\t");
-						break;
-					case Cell.CELL_TYPE_STRING:
-						System.out.print(cell.getStringCellValue() + "\t\t");
-						break;
-					}
 				}
+
 				System.out.println("");
 			}
 			file.close();
@@ -66,51 +76,46 @@ public class ReadWriteExcelFile {
 	}
 
 	public void writeXLSFile() {
-		
-		
-		//Blank workbook
-        XSSFWorkbook workbook = new XSSFWorkbook();
-         
-        //Create a blank sheet
-        XSSFSheet sheet = workbook.createSheet("Employee Data");
-          
-        //This data needs to be written (Object[])
-        Map<String, Object[]> data = new TreeMap<String, Object[]>();
-        data.put("1", new Object[] {"ID", "NAME", "LASTNAME"});
-        data.put("2", new Object[] {1, "Amit", "Shukla"});
-        data.put("3", new Object[] {2, "Lokesh", "Gupta"});
-        data.put("4", new Object[] {3, "John", "Adwards"});
-        data.put("5", new Object[] {4, "Brian", "Schultz"});
-          
-        //Iterate over data and write to sheet
-        Set<String> keyset = data.keySet();
-        int rownum = 0;
-        for (String key : keyset)
-        {
-            Row row = sheet.createRow(rownum++);
-            Object [] objArr = data.get(key);
-            int cellnum = 0;
-            for (Object obj : objArr)
-            {
-               Cell cell = row.createCell(cellnum++);
-               if(obj instanceof String)
-                    cell.setCellValue((String)obj);
-                else if(obj instanceof Integer)
-                    cell.setCellValue((Integer)obj);
-            }
-        }
-        try
-        {
-            //Write the workbook in file system
-            FileOutputStream out = new FileOutputStream(new File("C:\\testWrite.xlsx"));
-            workbook.write(out);
-            out.close();
-            System.out.println("testWrite.xlsx written successfully on disk.");
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+
+		// Blank workbook
+		XSSFWorkbook workbook = new XSSFWorkbook();
+
+		// Create a blank sheet
+		XSSFSheet sheet = workbook.createSheet("Employee Data");
+
+		// This data needs to be written (Object[])
+		Map<String, Object[]> data = new TreeMap<String, Object[]>();
+		data.put("1", new Object[] { "ID", "NAME", "LASTNAME" });
+		data.put("2", new Object[] { 1, "Amit", "Shukla" });
+		data.put("3", new Object[] { 2, "Lokesh", "Gupta" });
+		data.put("4", new Object[] { 3, "John", "Adwards" });
+		data.put("5", new Object[] { 4, "Brian", "Schultz" });
+
+		// Iterate over data and write to sheet
+		Set<String> keyset = data.keySet();
+		int rownum = 0;
+		for (String key : keyset) {
+			Row row = sheet.createRow(rownum++);
+			Object[] objArr = data.get(key);
+			int cellnum = 0;
+			for (Object obj : objArr) {
+				Cell cell = row.createCell(cellnum++);
+				if (obj instanceof String)
+					cell.setCellValue((String) obj);
+				else if (obj instanceof Integer)
+					cell.setCellValue((Integer) obj);
+			}
+		}
+		try {
+			// Write the workbook in file system
+			FileOutputStream out = new FileOutputStream(new File(
+					"C:\\testWrite.xlsx"));
+			workbook.write(out);
+			out.close();
+			System.out.println("testWrite.xlsx written successfully on disk.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
