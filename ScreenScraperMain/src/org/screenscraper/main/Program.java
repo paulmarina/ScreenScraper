1e61bea6ee8e9c40568991edd0819ba6870c1d57
alinamoldovan90 <alina.moldovan@fortech.ro>
2015-05-04 15:00:50 +0300

    added parser and object classes

--- ScreenScraperMain/src/org/screenscraper/main/Program.java
+++ ScreenScraperMain/src/org/screenscraper/main/Program.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 public class Program {
 
 	public static void main(String args[]) {
-		
-		try {
+		System.out.println("cevaaaaaaaa");
+		/*try {
 			System.in.read();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	}
 }
