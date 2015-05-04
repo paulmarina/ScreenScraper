306aa3b63a4e15b1b9b978d0d3fe9b23bc23f810
ioanafoidas <ioana.foidas@fortech.ro>
2015-05-04 15:02:23 +0300

    added excel package and poi jar

---
+++ ScreenScraperMain/src/org/screenscraper/excel/ReadWriteExcelFile.java
@@ -0,0 +1,17 @@
+package org.screenscraper.excel;
+
+public class ReadWriteExcelFile {
+	
+	
+	public void readXLSFile()
+	{
+		
+	}
+	
+	
+	public void writeXLSFile()
+	{
+		
+	}
+	
+}
--- ScreenScraperMain/src/org/screenscraper/main/Program.java
+++ ScreenScraperMain/src/org/screenscraper/main/Program.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 public class Program {
 
 	public static void main(String args[]) {
+
 		
-		try {
-			System.in.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+		  try { System.in.read(); 
+		  
+		  } catch (IOException e) 
+		  
+		  { // TODOAuto-generated catch block 
+			  
+			  e.printStackTrace(); }
+		  }
+		 
+
+	
 }
