5c1c3d30a3a5362abffa83abb2bee8776605a2ff
Paul Marina <paul.marina@fortech.ro>
2015-05-06 09:43:15 +0300

    Updated xml companies reader

--- ScreenScraperMain/src/org/screenscraper/objects/CompanyUrl.java
+++ ScreenScraperMain/src/org/screenscraper/objects/CompanyUrl.java
@@ -3,6 +3,15 @@ package org.screenscraper.objects;
 public class CompanyUrl {
 	
 	private String url;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public String getUrl() {
 		return url;
